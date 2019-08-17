fe2e7536f571a9c763cf09bc0b14f903f20ac77c
Resized <shai.yarden@gmail.com>
2019-08-17 22:17:01 +0300

    Implemented parallelization with MPI and OpenMP

--- InitialMPIproject/InitialMPIproject.cpp
+++ InitialMPIproject/InitialMPIproject.cpp
@@ -6,20 +6,21 @@
 *  (C) 2001 by Argonne National Laboratory.
 *      See COPYRIGHT in top-level directory.
 */
+#define MAX_POINTS 500000
+#define MAX_DIMENTIONS 20
+
+const char* INPUT_FILE = "C:\\ParallelProject\\input.txt";
+const char* OUTPUT_FILE = "C:\\ParallelProject\\output.txt";
 
 /* This is an interactive version of cpi */
 #include <mpi.h>
 #include <stdio.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <vector>
 #include <omp.h>
 #include <math.h>
 
-
-#define HEAVY 10000000
-
-
-using namespace std;
+//using namespace std;
 
 //•	N - number of points
 //•	K – number of coordinates of points
@@ -33,25 +34,22 @@ typedef struct {
 	int set;
 } POINT;
 
-float * weights;
 
-int readPointFromFile(FILE * fp, POINT * points, int * pointNum, int K)
+int readPointFromFile(FILE * fp, POINT * points, int pointIndex, int K)
 {
 	if (feof(fp))
 	{
 		return 0;
 	}
 
-	//#pragma omp parallel for
 	for (int i = 0; i < K; i++)
 	{
-		if (!fscanf(fp, "%f ", &(points[*pointNum].x[i])))
+		if (!fscanf(fp, "%f ", &(points[pointIndex].x[i])))
 			return 0;
 	}
-	points[*pointNum].x[K] = 1;
-	if (!fscanf(fp, "%d \n", &(points[*pointNum].set)))
+	points[pointIndex].x[K] = 1;
+	if (!fscanf(fp, "%d \n", &(points[pointIndex].set)))
 		return 0;
-	(*pointNum)++;
 	return 1;
 }
 
@@ -66,7 +64,6 @@ int sign(float num)
 float func(float * x, float * weights, int K)
 {
 	float result = 0;
-//#pragma omp parallel for reduction(+:result)
 	for (int i = 0; i < K + 1; i++)
 	{
 		result += weights[i] * x[i];
@@ -76,47 +73,44 @@ float func(float * x, float * weights, int K)
 
 void updateWeights(float * x, float * weights, int signResult, float alpha, int K)
 {
-#pragma omp parallel for
 	for (int i = 0; i < K + 1; i++)
 	{
 		weights[i] = weights[i] + alpha*-signResult*x[i];
 	}
 }
 
-void zeroWeights(float * weights, int K)
+void zeroWeights(float ** weights, int K, int numOfWeights)
 {
-#pragma omp parallel for
-	for (int i = 0; i < K + 1; i++)
+	for (int i = 0; i < numOfWeights; i++)
 	{
-		weights[i] = 0;
+		memset(weights[i], 0, sizeof(float)*(K + 1));
 	}
 }
 
-void heavy() {
-	int i;
-	for (i = 0; i < HEAVY; i++)
-		exp(sin(exp(sin(exp(-2.)))));
+int isMiss(int &signResult, POINT * points, int i, float * weights, int K, int &Nmis)
+{
+	signResult = sign(func(points[i].x, weights, K));
+	if (signResult != points[i].set)
+	{
+		return 1;
+	}
+	return 0;
 }
 
-unsigned long getLineSize(FILE * fp, int K)
+void freeMemory(int N, POINT * points, int numOfSegments, float ** weights, bool * isSegmentClassifiedProperly, char * buffer)
 {
-	fflush(fp);
-	unsigned long firstPos = ftell(fp);
-	float temp;
-	int temp2;
-	if (feof(fp))
-		return 0;
-
-	for (int i = 0; i < K; i++)
-		if (!fscanf(fp, "%f ", &temp))
-			return 0;
-
-	if (!fscanf(fp, "%d \n", &temp2))
-		return 0;
-
-	fflush(fp);
-	return ftell(fp) - firstPos;
-
+	for (int i = 0; i < N; i++)
+	{
+		free(points[i].x);
+	}
+	for (int i = 0; i < numOfSegments; i++)
+	{
+		free(weights[i]);
+	}
+	free(weights);
+	free(points);
+	free(isSegmentClassifiedProperly);
+	free(buffer);
 }
 
 int main(int argc, char *argv[])
@@ -134,149 +128,257 @@ int main(int argc, char *argv[])
 
 	MPI_Status status;
 
-
 	float alpha, alphaZero, alphaMax, QC, quality;
-	int N, Nmis = 0, K, LIMIT, iterCounter = 0, pointNum = 0;
-	bool isClassifiedProperly = false;
+	int N, Nmis = 0, K, LIMIT, pointNum = 0;
 	bool isAlphaFound = false;
 	double t1, t2;
 	POINT * points;
 	FILE * fp;
-	float * x;
-	int * y;
+	float ** weights;
+	int numOfSegments = 16;
+	char * buffer;
 
-	fp = fopen("data1.txt", "r");
-	if (fp == NULL)
+	//if (numprocs < 2)
+	//{
+	//	printf("Number of processes must be greater than 1\n");
+	//	MPI_Abort(MPI_COMM_WORLD, 0);
+	//}
+
+	if (myid == 0)
 	{
-		printf("file could not be opened\n");
-		MPI_Finalize();
-		exit(EXIT_FAILURE);
+		fp = fopen(INPUT_FILE, "r");
+		if (fp == NULL)
+		{
+			printf("file could not be opened for reading\n");
+			MPI_Finalize();
+			exit(EXIT_FAILURE);
+		}
+
+		// N    K    alphaZero   alphaMax LIMIT   QC
+		fscanf(fp, "%d %d %f %f %d %f \n", &N, &K, &alphaZero, &alphaMax, &LIMIT, &QC);
+		//MPI_Recv(&answer, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
+	}
+
+	MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&K, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&alphaZero, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&alphaMax, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&LIMIT, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&QC, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+	points = (POINT*)malloc(sizeof(POINT) * N);
+	weights = (float**)malloc(sizeof(float*) * numOfSegments);
+	bool * isSegmentClassifiedProperly = (bool*)calloc(numOfSegments, sizeof(bool));
+	for (int i = 0; i < numOfSegments; i++)
+	{
+		weights[i] = (float*)calloc(K + 1, sizeof(float));
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		points[i].x = (float*)malloc(sizeof(float) * (K + 1));
+	}
+
+	buffer = (char*)malloc(sizeof(float) * N * (K + 2));
+	int BUFFER_SIZE = sizeof(float) * N * (K + 2);
+	int position;
+
+	if (myid == 0)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			readPointFromFile(fp, points, i, K);
+		}
+		fclose(fp);
 	}
 
 	t1 = MPI_Wtime();
 
-	// N    K    alphaZero   alphaMax LIMIT   QC
-	fscanf(fp, "%d %d %f %f %d %f \n", &N, &K, &alphaZero, &alphaMax, &LIMIT, &QC);
-
-	x = (float*)malloc(sizeof(float) * N * (K + 1));
-	y = (int*)malloc(sizeof(int)* N);
-
-	points = (POINT*)malloc(sizeof(POINT) * N);
-	weights = (float*)calloc(K + 1, sizeof(float));
-
-	//#pragma omp parallel for
-	for (int i = 0; i < N; i++)
+	if (myid == 0)
 	{
-		points[i].x = (float*)malloc(sizeof(double) * (K + 1));
-	}
-
-	unsigned long position, offset;
-	fflush(fp);
-	position = ftell(fp);
-
-	offset = getLineSize(fp, K);
-
-	fseek(fp, position + offset*10, SEEK_SET);
-
-	//#pragma omp parallel for
-	for (int i = 0; i < N; i++)
-	{
-		//fseek(fp, position + offset*i, SEEK_SET);
-		readPointFromFile(fp, points, &pointNum, K);
-	}
-
-	fclose(fp);
-
-	alpha = alphaZero;
-	int signResult = 0;
-
-	while (true)
-	{
-		zeroWeights(weights, K);
-		while (!isClassifiedProperly && iterCounter < LIMIT)
+		position = 0;
+		for (int i = 0; i < N; i++)
 		{
-			iterCounter++;
-			isClassifiedProperly = true;
-			//#pragma omp parallel for
-			for (int i = 0; i < N; i++)
+			MPI_Pack(points[i].x, K + 1, MPI_FLOAT, buffer, BUFFER_SIZE, &position, MPI_COMM_WORLD);
+			MPI_Pack(&points[i].set, 1, MPI_INT, buffer, BUFFER_SIZE, &position, MPI_COMM_WORLD);
+		}
+	}
+	MPI_Bcast(&position, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(buffer, position, MPI_PACKED, 0, MPI_COMM_WORLD);
+	if (myid != 0)
+	{
+		position = 0;
+		for (int i = 0; i < N; i++)
+		{
+			MPI_Unpack(buffer, BUFFER_SIZE, &position, points[i].x, K + 1, MPI_FLOAT, MPI_COMM_WORLD);
+			MPI_Unpack(buffer, BUFFER_SIZE, &position, &points[i].set, 1, MPI_INT, MPI_COMM_WORLD);
+		}
+	}
+
+	//for (int i = 0; i < N; i++) 
+	//{
+	//	MPI_Bcast(points[i].x, K + 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	//	MPI_Bcast(&points[i].set, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	//}
+
+	
+
+	// Give each process an alpha range to work on
+	float alpha_low = alphaZero + myid * ((alphaMax-alphaZero) / numprocs);
+	float alpha_high = alphaZero + (myid + 1) * ((alphaMax - alphaZero) / numprocs);
+	//QC = 0.01f;
+	for (alpha = alpha_low; alpha < alpha_high; alpha += alphaZero)
+	{
+		zeroWeights(weights, K, numOfSegments);
+		bool isClassifiedProperly = false;
+		int iterCounter = 0;
+
+		#pragma omp parallel
+		{
+			while (!isClassifiedProperly && iterCounter < LIMIT)
 			{
-				signResult = sign(func(points[i].x, weights, K));
-				if (signResult != points[i].set)
+				#pragma omp for
+				for (int i = 0; i < numOfSegments; i++)
 				{
-					updateWeights(points[i].x, weights, signResult, alpha, K);
-					isClassifiedProperly = false;
-					break;
+					isSegmentClassifiedProperly[i] = true;
+					int start = i * (N / numOfSegments);
+					int finish = i == numOfSegments - 1 ? N : (i + 1) * (N / numOfSegments);
+					for (int j = start; j < finish; j++)
+					{
+						if (isSegmentClassifiedProperly[i])
+						{
+							int signResult = sign(func(points[i].x, weights[i], K));
+							if (signResult != points[i].set)
+							{
+								updateWeights(points[i].x, weights[i], signResult, alpha, K);
+								isSegmentClassifiedProperly[i] = false;
+								break;
+							}
+						}
+					}
+				}
+
+				#pragma omp single
+				{
+					for (int i = 1; i < numOfSegments; i++) // Averaging the weights
+					{
+						for (int j = 0; j < K + 1; j++)
+						{
+							weights[0][j] += (weights[i][j] - weights[0][j]) / (i + 1);
+						}
+					}
+
+					for (int i = 1; i < numOfSegments; i++) // Reset all weights to be the same as the averaged weight 0
+					{
+						memcpy(weights[i], weights[0], sizeof(float)*(K + 1));
+					}
+					isClassifiedProperly = true;
+					for (int i = 0; i < numOfSegments; i++) // Check if every segment is classified properly, if so then end
+					{
+						if (isSegmentClassifiedProperly[i] == false)
+						{
+							isClassifiedProperly = false;
+							break;
+						}
+					}
+					iterCounter++;
 				}
 			}
 		}
+
 		Nmis = 0;
-		//#pragma omp parallel for
-		for (int i = 0; i < N; i++)
+		#pragma omp parallel for reduction(+:Nmis)
+		for (int i = 0; i < N; i++) // Count number of misses for evaluation of quality 
 		{
-			signResult = sign(func(points[i].x, weights, K));
-			if (signResult != points[i].set)
-			{
-				Nmis++;
-			}
+			int signResult = 0;
+			Nmis += isMiss(signResult, points, i, weights[0], K, Nmis);
 		}
+
 		quality = (float)Nmis / N;
 
 		if (quality < QC)
 		{
+			isAlphaFound = true;
 			break;
 		}
-		alpha += alphaZero;
-		if (alpha > alphaMax)
-		{
-			break;
-		}
+		//alpha += alphaZero;
 	}
+	float min_quality = 0;
+
+	MPI_Reduce(&quality, &min_quality, 1, MPI_FLOAT, MPI_MIN, 0, MPI_COMM_WORLD);
+
+	//quality = min_quality;
 
 	t2 = MPI_Wtime();
 
-	printf("minimal alpha value: %1.4f\n", alpha);
-	printf("quality value: %1.5f\n", quality);
-	printf("weight 0: %1.4f\n", weights[0]);
-	printf("weight 1: %1.4f\n", weights[1]);
-	printf("weight 2: %1.4f\n", weights[2]);
-
-	printf("MPI_Wtime: %1.6f seconds\n", t2 - t1);// fflush(stdout);
-
-	// free memory
-	for (int i = 0; i < N; i++)
+	if (myid == 0)
 	{
-		free(points[i].x);
+		fp = fopen(OUTPUT_FILE, "w");
+		if (fp == NULL)
+		{
+			printf("file could not be opened for writing\n");
+			freeMemory(N, points, numOfSegments, weights, isSegmentClassifiedProperly, buffer);
+			MPI_Finalize();
+			exit(EXIT_FAILURE);
+		}
+
+		//// Print results to CMD 
+		//if (isAlphaFound)
+		//{
+		//	printf("Alpha minimum: %1.4f\n", alpha);
+		//	for (int i = 0; i < K + 1; i++)
+		//	{
+		//		printf("W%d: %1.4f\n", i + 1, weights[0][i]);
+		//	}
+		//	printf("q: %1.5f\n", quality);
+
+		//	printf("\nMPI_Wtime: %1.6f seconds\n", t2 - t1);
+		//}
+		//else
+		//{
+		//	printf("Alpha is not found\n");
+		//}
+
+		// Print results to output.txt
+		if (isAlphaFound)
+		{
+			fprintf(fp, "Alpha minimum: %1.4f\n", alpha);
+			for (int i = 0; i < K + 1; i++)
+			{
+				fprintf(fp, "%1.4f\n", weights[0][i]);
+			}
+			fprintf(fp, "%1.5f\n", quality);
+
+			fprintf(fp, "\nMPI_Wtime: %1.6f seconds\n", t2 - t1);// fflush(stdout);
+		}
+		else
+		{
+			fprintf(fp, "Alpha is not found\n");
+		}
+
+		fclose(fp);
 	}
 
-	free(points);
-	free(weights);
+	// Print results to CMD 
+	if (isAlphaFound)
 
-//	double start_time, end_time, tick;
-//	start_time = omp_get_wtime();
-//
-//#pragma omp parallel for
-//	for (int i = 0; i < 4; i++)
-//	{
-//		heavy();
-//	}
-//	heavy();
-//	end_time = omp_get_wtime();
-//	printf("Time %lf\n", end_time - start_time);
+	{
+		printf("\nAlpha minimum: %1.4f\n", alpha);
+		for (int i = 0; i < K + 1; i++)
+		{
+			printf("W%d: %1.4f\n", i + 1, weights[0][i]);
+		}
+		printf("q: %1.5f\n", quality);
 
+		printf("\nMPI_Wtime: %1.6f seconds\n", t2 - t1);
+	}
+	else
+	{
+		printf("Alpha is not found\n");
+	}
 
-	//int x = 7, y = 10, answer = 77777;
-	//if (myid == 0) {
-	//	MPI_Send(&x, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-	//	MPI_Recv(&answer, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &status);
-	//}
-	//else {
-	//	MPI_Recv(&y, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-	//	y = y * 3;
-	//	MPI_Send(&y, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-	//}
-
-	//if (myid == 0)
-	//printf("answer = %d numprocs = %d  myid = %d   %s\n", answer, numprocs, myid, processor_name);
+	// free memory
+	freeMemory(N, points, numOfSegments, weights, isSegmentClassifiedProperly, buffer);
 
 	MPI_Finalize();
 	exit(EXIT_SUCCESS);
